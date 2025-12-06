5f558fcc08d68b738cc99b6d6a35cd3f76cb2c43
Joel <joel.jppz@gmail.com>
2025-12-06 15:55:57 -0600

    initial commit - Spring Boot base project

---
+++ WiredHub/src/main/java/com/comicWeb/WiredHub/WiredHubApplication.java
@@ -0,0 +1,13 @@
+package com.comicWeb.WiredHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiredHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiredHubApplication.class, args);
+	}
+
+}
---
+++ WiredHub/src/test/java/com/comicWeb/WiredHub/WiredHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comicWeb.WiredHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiredHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
